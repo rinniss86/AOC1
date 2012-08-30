22e20dec5a21cd888482b5199bc6b091a9f6c5cf
rinniss86 <rinniss86@gmail.com>
2012-08-29 21:26:25 -0400

    Added project files

---
+++ testApp/testApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Richard Inniss on 8/29/12.
+//  Copyright (c) 2012 Richard Inniss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testApp/testAppTests/testAppTests.h
@@ -0,0 +1,13 @@
+//
+//  testAppTests.h
+//  testAppTests
+//
+//  Created by Richard Inniss on 8/29/12.
+//  Copyright (c) 2012 Richard Inniss. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testAppTests : SenTestCase
+
+@end
